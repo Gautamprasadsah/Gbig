88c40cf70b48aebb42c4d524d1517fabee636fa3
Gautam <46226462+Gautamprasadsah@users.noreply.github.com>
2022-07-01 09:06:59 +0545

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,36 @@
+<?php
+//conect to database
+$servername = "localhost";
+$username = "root";
+$password = "";
+$db = "test2";
+$conn = mysqli_connect($servername, $username, $password,$db);
+ 
+//checking connection  
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+else{
+echo "connected";
+}
+//insert
+$name = $_POST['name']; 
+$email = $_POST['email'];
+$phonenumber = $_POST['phonenumber']; 
+$message = $_POST['message'];   
+
+$run = "INSERT INTO `test2`(name, email, phonenumber, message) VALUES ( '$name' , '$email' , '$phonenumber' , '$message' )";
+$result = mysqli_query( $conn , $run) ;
+if($result)
+{
+	echo "<br><br><br><br><br><br><br><br><h1 align='center'>Yours Details are Submitted...!!</h1>";
+        echo "<br><br><br><br><br><br><br><br><h1 align='center'>Thank You...!!</h1>";
+        header( "refresh:3;url=contact.html" );
+    
+}
+else
+{
+	echo "not registered";
+} 
+
+?>
